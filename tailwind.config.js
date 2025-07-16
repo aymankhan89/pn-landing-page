5d917efec8d804f67e9f50f7fc8a03a4ff16448c
ayan-pnow <arazvi@proficientnowtech.com>
2025-07-16 16:44:25 +0530

    font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+
+
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {fontFamily: {
-              },},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+   content: [
+        './src/**/*.{html,js}',
+      ],
+  theme: {
+    extend: { fontFamily: {
+       inter11: ['Inter', ...defaultTheme.fontFamily.sans],
+    } },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
